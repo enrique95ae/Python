2fb59fe32cf9d3be70981667608f6f132b750b43
enrique95ae <enrique95ae@gmail.com>
2019-08-30 22:22:35 -0700

    Create test004.py

---
+++ Project 01/test004.py
@@ -0,0 +1,11 @@
+##list of integers
+student_score= [99, 88, 60]
+
+##printing out that list
+print(student_score)
+
+##printing all the integers in a range
+print(list(range(1,10)))
+
+##printing out all the integers in a range skipping one every time
+print(list(range(1,10,2)))
