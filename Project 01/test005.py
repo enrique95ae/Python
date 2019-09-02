890aede1b0cc3e0625c0b2bb5aab4428553b63a3
enrique95ae <enrique95ae@gmail.com>
2019-09-02 06:42:55 +0200

    Update test005.py

--- Project 01/test005.py
+++ Project 01/test005.py
@@ -2,6 +2,6 @@ student_scores = [9.9, 9.0, 3.4]
 
 mysum = sum(student_scores)
 num_of_students = len(student_scores)
-average = mysum/num_of_students
+average = mysum / num_of_students
 
 print(average)
