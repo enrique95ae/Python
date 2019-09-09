c9c16959e9d90392226a13a0e53baefe9a77b07b
enrique95ae <enrique95ae@gmail.com>
2019-09-10 01:43:46 +0200

    Update test007.py

---
+++ Project 01/test007.py
@@ -0,0 +1,10 @@
+monday_temperatures = [9.1, 8.8, 7.5, 6.6, 8.9]
+
+##printing the number of items in the list
+print(len(monday_temperatures))
+
+##printing the item with index 3
+print(monday_temperatures[3])
+
+##printing the items between index 1 and 4
+print(monday_temperatures[1:4])
