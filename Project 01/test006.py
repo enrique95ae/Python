032c52fba177f5a4923e0e057fec468c999ed9e0
enrique95ae <enrique95ae@gmail.com>
2019-09-09 20:27:49 +0200

    Create test006.py

---
+++ Project 01/test006.py
@@ -0,0 +1,9 @@
+#tuple: unmutable
+monday_temperatures = (1, 4, 5)
+
+#list: mutable
+monday_temperatures2 = [1, 4, 5]
+monday_temperatures2.append(6)
+
+print(monday_temperatures)
+print(monday_temperatures2)
