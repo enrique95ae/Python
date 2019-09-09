2024034bf4768514f2f423535d37d02526eedf3a
enrique95ae <enrique95ae@gmail.com>
2019-09-09 20:22:55 +0200

    .

--- Project 01/test005.py
+++ Project 01/test005.py
@@ -1,7 +1,18 @@
-student_scores = [9.9, 9.0, 3.4]
+student_scores_list = [9.9, 9.0, 3.4]
+student_scores_dictionary = {"Jack": 9.9, "John": 9.0, "Jim": 3.4}
 
-mysum = sum(student_scores)
-num_of_students = len(student_scores)
+
+##calculating the average grade in the list
+mysum = sum(student_scores_list)
+num_of_students = len(student_scores_list)
+average = mysum / num_of_students
+
+print(average)
+
+
+##calculating the average grade in the dictionary
+mysum = sum(student_scores_dictionary.values())
+num_of_students = len(student_scores_dictionary)
 average = mysum / num_of_students
 
 print(average)
