738f5289c58dd8a82274257ac4812c02cbde0bcf
enrique95ae <enrique95ae@gmail.com>
2019-09-12 05:56:49 +0200

    Create test009.py

---
+++ Project 01/test009.py
@@ -0,0 +1,10 @@
+#defining a list
+student_grades = [9.0, 8.5, 5.4]
+
+#making a function to calculate the mean value of a list (sent as parameter)
+def mean(theList):
+    the_mean = sum(theList) / len(theList)
+    return the_mean
+
+#printing the result of calling the mean function previously created
+print(mean(student_grades))
