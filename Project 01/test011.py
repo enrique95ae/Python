a493f0807071402481a0e6eeccdac067291c9e8f
enrique95ae <enrique95ae@gmail.com>
2019-09-14 09:26:26 +0200

    Update test011.py

--- Project 01/test011.py
+++ Project 01/test011.py
@@ -1,7 +1,9 @@
 def weather_condition(temperature):
-    if temperature > 7:
+    if temperature >= 7:
         return "Warm"
     else:
         return "Cold"
 
-print(weather_condition(5))
+inputTemp = float(input("Enter temperature:"))
+
+print(weather_condition(inputTemp))
