45ba15c7d34ef8cc18ff479a8d9b259b9fc84ca9
enrique95ae <enrique95ae@gmail.com>
2019-09-15 08:21:22 +0200

    Update test014.py

--- Project 01/test014.py
+++ Project 01/test014.py
@@ -2,6 +2,8 @@ monday_temperatures = [9.1, 8.8, 7.6]
 
 for i in monday_temperatures:    
     print(round(i))
+print("First for loop done")
 
 for letter in "Hello World!":
     print(letter)
+print("Second for loop done")
